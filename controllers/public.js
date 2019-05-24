eaa6b803a10112469b9ca970a1205a2607fac134
Luther <luther0211@hotmail.com>
2019-05-23 22:51:35 -0500

    added search data bar under navbar

--- controllers/public.js
+++ controllers/public.js
@@ -32,19 +32,23 @@ exports.getSearch = (req, res, next) => {
                     res.status(404).redirect('/not-found')
                 }
 
-                console.log(result.GoodreadsResponse.Request)
-                console.log(result.GoodreadsResponse.search)
+                // console.log(result.GoodreadsResponse.Request)
+                console.log(result.GoodreadsResponse.search[0])
                 
                 const books = result.GoodreadsResponse.search[0].results[0].work.map(book => {
                     return {
                         title: book.best_book[0].title[0],
                         image_url: book.best_book[0].image_url[0],
-                        author: book.best_book[0].author[0].name[0],
+                        author: book.best_book[0].author[0].name[0]
                     }
                 })
                 
                 res.render('public/result', {
-                    books
+                    books,
+                    searchQuery: result.GoodreadsResponse.search[0].query[0],
+                    numOfResults: result.GoodreadsResponse.search[0]['total-results'][0],
+                    resultStart: result.GoodreadsResponse.search[0]['results-start'][0],
+                    resultEnd: result.GoodreadsResponse.search[0]['results-end'][0]
                 })
 
             })
